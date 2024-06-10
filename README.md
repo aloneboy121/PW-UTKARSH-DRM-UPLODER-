e22150e60134e57f7352e615cd6078da2ba64530
Rafter148 <165582023+Rafter148@users.noreply.github.com>
2024-06-10 09:37:45 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# PW-UTKARSH-DRM-UPLODER-
-PW UTKARSH DRM UPLODER 
+
+
+
+## Deploy To Heroku
+
+<a href="https://heroku.com/deploy?template=https://github.com/Krishna3000300/utkarsh-pw-drm-UPLODER-">
+     <img height="30px" src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku">
+  </a>
+
---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+API_ID = API_ID = 24119778
+
+API_HASH = os.environ.get("API_HASH", "cca11ca97dd8683d65ca1beb62baceb1")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7208534557:AAH9zDoXCjMJkm8ahqsJTsljAzbeSe20Xic")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 672225212))
+STRING = int(os.environ.get("STRING", AQFwCeIAOz1FS7JecfYU8zMZdNCoey8c3cbpOlG0CmPPY9mXBXyG2C0_Uf83cWS_dI38I16qpCyuggIpwc2LrcYQtUXEbyjxtfRWl3jif61NDbq95dMqvSLJkYz6xGaPas5qCfMubSwdkxgaFc_ejU1A5Fglp4RsPKnH_G4OjF_wEUAzNiWn0PhTVlUT-au6thdjfR-hsPvGUnksl4bTRb4BfR0cFsh0X8Tg14_KGJRMBGUN2URG1PO1ATGpiFDPmUDFco2SwQIW6VadpUUROTUD70z0SnAvQ7aZMD7MvpovTdHO8YJVu0GiMT5qJZKtNGHUzKj4rdxiKdHoA0_9z5h2pIR20QAAAAGrb7QLAA))
+
+LOG = -1002074904583
+
